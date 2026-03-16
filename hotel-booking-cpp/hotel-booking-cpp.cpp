7e5b655e40b036066243d29a41883cef99f8beaa
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-16 15:17:22 +0300

    feat: add data types - enums and structs

--- hotel-booking-cpp/hotel-booking-cpp.cpp
+++ hotel-booking-cpp/hotel-booking-cpp.cpp
@@ -1,20 +1,31 @@
-// hotel-booking-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
 
-#include <iostream>
+enum enRoomType { Single = 1, Double = 2, Suite = 3 };
+enum enBookingStatus { Available = 1, Booked = 2 };
+
+struct stRoom
+{
+    unsigned short RoomNumber = 0;
+    enRoomType Type;
+    double PricePerNight = 0;
+    enBookingStatus Status = enBookingStatus::Available;
+};
+
+struct stBooking
+{
+    unsigned short BookingID = 0;
+    stRoom Room;
+    string GuestName = "";
+    string CheckInDate = "";
+    string CheckOutDate = "";
+    unsigned short NumberOfNights = 0;
+    double TotalPrice = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
